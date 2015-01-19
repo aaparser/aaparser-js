2b85e3c69e4651b24b4573ff8c102325813adac8
Harald Lapp <h.lapp@clipdealer.de>
2015-01-19 15:05:15 +0100

    added comments and initialization (for some coercion functions)

--- src/coercion.js
+++ src/coercion.js
@@ -6,17 +6,39 @@
  */
 
 module.exports = {
-    collect: function(value, collection) {
+    /**
+     * Build a collection (array) of values.
+     */
+    collect: function(value, collection)
+    {
+        if (Object.prototype.toString.call(settings.default) !== '[object Array]') {
+            collection = [];
+        }
+        
         collection.push(value);
 
         return collection;
     },
 
+    /**
+     * Increase a counter.
+     */
     count: function(value, total) {
+        if (isNaN(total)) {
+            total = 0;
+        }
+        
         return total += value;
     },
 
+    /**
+     * Build a key/value with values provided.
+     */
     kv: function(value, collection) {
+        if (Object.prototype.toString.call(settings.default) !== '[object Array]') {
+            collection = {};
+        }
+        
         var kv = value.split('=');
 
         collection[k[0]] = v[0];
@@ -24,14 +46,23 @@ module.exports = {
         return collection;
     },
 
+    /**
+     * Split a value by ','.
+     */
     list: function(value) {
-        return value.split(',');
+        return value.split(/ *, */);
     },
 
+    /**
+     * Build a range of numbers.
+     */
     range: function(value) {
-        return value.split('..');
+        return value.split('..').map(Number);
     },
 
+    /**
+     * Just store the value.
+     */
     value: function(value) {
         return value;
     }
